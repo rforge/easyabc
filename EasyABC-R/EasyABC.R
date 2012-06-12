aec0e06693580628b72dd5e069560b5f7e33ef9b
dumoulin <dumoulin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-12 09:42:53 +0000

    * two functions merged with a use_seed option  * specific function for binary model
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/easyabc@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- EasyABC-R/EasyABC.R
+++ EasyABC-R/EasyABC.R
@@ -1,7 +1,6 @@
-ABC_launcher<-function(model,prior_matrix,nb_simul){
+ABC_launcher<-function(model,prior_matrix,nb_simul,use_seed=FALSE){
 	tab_simul_summarystat=NULL
 	tab_param=NULL
-	
 	
 	for (i in 1:nb_simul){
 		l=dim(prior_matrix)[1]
@@ -9,41 +8,28 @@ ABC_launcher<-function(model,prior_matrix,nb_simul){
 		for (j in 1:l){
 			param[j]=runif(1,min=prior_matrix[j,1],max=prior_matrix[j,2])
 		}
-		write.table(param,file="input",row.names=F,col.names=F,quote=F)
-		system(model)
-		simul_summarystat=read.table("output",h=F)
-		tab_simul_summarystat=rbind(tab_simul_summarystat,simul_summarystat)
-		tab_param=rbind(tab_param,param)
-	}
-cbind(tab_param,tab_simul_summarystat)
-}
-
-
-ABC_launcher_with_seed<-function(model,prior_matrix,nb_simul){
-	tab_simul_summarystat=NULL
-	tab_param=NULL
-	
-	
-	for (i in 1:nb_simul){
-		l=dim(prior_matrix)[1]
-		param=array(0,l)
-		for (j in 1:l){
-			param[j]=runif(1,min=prior_matrix[j,1],max=prior_matrix[j,2])
+		if (use_seed) {
+			param=c(i,param)
 		}
-		param=c(i,param)
-		write.table(param,file="input",row.names=F,col.names=F,quote=F)
-		system(model)
-		simul_summarystat=read.table("output",h=F)
+		simul_summarystat=model(param)
 		tab_simul_summarystat=rbind(tab_simul_summarystat,simul_summarystat)
 		tab_param=rbind(tab_param,param)
 	}
-cbind(tab_param,tab_simul_summarystat)
+	cbind(tab_param,tab_simul_summarystat)
 }
 
+binary_model<-function(command) {
+	invoke<-function(param) {
+		write.table(param,file="input",row.names=F,col.names=F,quote=F)
+		system(command)
+		read.table("output",h=F)
+	}
+}
 
+# sample usage
 
 prior_matrix=c(1,1,-1,1000,10000,100,100,1,1000,10000)
 dim(prior_matrix)<-c(5,2)
 prior_matrix
 
-ABC_launcher_with_seed("parthy_test.exe",prior_matrix,10)
+ABC_launcher(binary_model("./parthy"),prior_matrix,10,TRUE)
