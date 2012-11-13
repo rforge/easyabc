0088e54c08393874930d15542d2ca279e212390c
dumoulin <dumoulin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-13 12:33:58 +0000

    basic webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/easyabc@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,19 +33,60 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<h1>EasyABC: a R package to perform efficient approximate Bayesian computation sampling schemes</h1>
 
-<!-- end of project description -->
+<h2>About EasyABC</h2>
 
-<p> No content added. </p>
+<p>The package EasyABC enables to launch a series of simulations of a computer code from the R platform, and to retrieve the simulation outputs in an appropriate format for post-processing treatments.</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p>Four sequential sampling schemes and three coupled-to-MCMC schemes are implemented. EasyABC further enables to launch the simulations in parallel on multiple cores of a computer.</p>
+
+<p><b>Package maintainer:</b> <a href="mailto:nicolas.dumoulin@irstea.fr">Nicolas Dumoulin</a></p>
+<p><b>Developers:</b> <a href="mailto:franck.jabot@irstea.fr">Franck Jabot, <a href="mailto:thierry.faure@irstea.fr">Thierry Faure</a>, <a href="mailto:nicolas.dumoulin@irstea.fr">Nicolas Dumoulin</a></p>
+
+<p>Development of EasyABC has been supported by the Irstea project DynIndic and by the French National Research Agency (ANR) within the SYSCOMM project DISCO (ANR-09-SYSC-003).</p>
+
+<p>Thanks to R-Forge for hosting the project.</p>
+
+<h2>News</h2>
+<ul>
+  <!--li>11/13/2012 EasyABC 1.0 is now available on CRAN.</li-->
+  <li>11/13/2012 EasyABC is now hosted on <a href="http://r-forge.r-project.org/projects/easyabc/">R-Forge</a>.</li>
+</ul>
+
+<h2>Current features</h2>
+<ul>
+  <li>Launching of computer simulations to perform sequential or coupled-to-MCMC sampling schemes for Approximate Bayesian Computation.</li>
+  <li>EasyABC currently implements four sequential algorithms:
+  <ul>
+    <li>Beaumont et al. Biometrika (2009)</li>
+    <li>Drovandi &amp; Pettit Biometrics (2011)</li>
+    <li>Del Moral et al. Statistics and Computing (2012)</li>
+    <li>Lenormand et al. ArXiv (2012)</li>
+  </ul></li>
+  <ul>
+    <li>EasyABC also implements three coupled-to-MCMC schemes:
+    <li>Marjoram et al. Pnas (2003)</li>
+    <li>Wegmann et al. Genetics (2009)</li>
+    <li>A mix of Marjoram and Wegmann's algorithms (Marjoram with calibration step – Wegmann without PLS)</li>
+  </ul></li>
+  <li>EasyABC additionally enables to launch computer simulations in parallel on multiple cores of a multi-core computer.</li>
+  <li>EasyABC also contains two R wrappers for binary codes to ease their launching from the R platform.
+</ul></li>
+
+<h2>Obtaining EasyABC</h2>
+
+<p>Version 1.0 is available on CRAN. Simply type install.packages("EasyABC") from within R.</p>
+
+<h2>Obtaining help and more information about EasyABC</h2>
+
+<p>There is a package vignette with more information about EasyABC. Simply type vignette("EasyABC") to view within R.</p>
+
+<p>If you have problems or questions about the code, please read the function documentation ( help(EasyABC) ).</p>
+
+<h2>Release history</h2>
+
+<!--p>Version 1.0 released on 13-12-2012.</p-->
 
 </body>
 </html>
