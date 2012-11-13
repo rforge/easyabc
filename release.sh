c954bb4dd1e0dd603ef94222375bf230d8cf85f1
dumoulin <dumoulin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-13 15:15:48 +0000

    directories reorganisation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/easyabc@205 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DATE=$(date +%Y-%m-%d)
+[ $# -eq 1 ] && VERSION=$1 ||
+VERSION=$(grep 'Version:'  pkg/DESCRIPTION | cut -d' ' -f2)
+
+echo "The package will be build with the version ${VERSION}"
+read -p "Are you sure? (y/n) " REPLY
+[ $REPLY"" != "y" ]] && {
+  read -p "Give the wanted version: " VERSION
+}
+
+sed -i -e 's/^Date:.*$/Date: '${DATE}'/' pkg/DESCRIPTION EasyABC/man/EasyABC-package.Rd
+sed -i -e 's/^Version:.*$/Version: '${VERSION}'/' pkg/DESCRIPTION EasyABC/man/EasyABC-package.Rd
+
+R CMD check pkg && R CMD build pkg
